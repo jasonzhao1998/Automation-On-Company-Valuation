a54c90c03cbf05094a85df893f804af05308205f
Jason Zhao/LGEVU Smart Program Management(jason.zhao@lge.com) <jason.zhao@lge.com>
2019-05-20 11:07:59 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import pandas as pd
+
+FILENAME = "Nvidia.xlsx"
+
+
+def main():
+    df = pd.read_excel('asset/' + FILENAME, header=4, index_col=0)
+    print(df)
+    print(df.dtypes)
+    print(df.index)
+    print(df.columns)
+
+if __name__ == "__main__":
+    main()
